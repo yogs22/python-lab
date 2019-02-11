1c8e04197b8db294e2c3027124610e5882f8a0f4
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-02-12 05:55:16 +0700

    Peewee get data

--- database/user.py
+++ database/user.py
@@ -12,19 +12,6 @@ class User(Model):
 sqlite_db.connect()
 sqlite_db.create_tables([User], safe = True)
 
-# Insert data
-# Insert one data
-
-# user = User(name = 'Yogi Testing 2', email = 'yogi@example.com')
-# user.save()
-
-# print(user)
-
-# Insert many data
-fields = [User.name, User.email]
-data   = [
-			('Yogi Test 8', 'yogs@example1.com'),
-			('Yogi Test 9', 'yogs@example2.com')
-		]
-
-User.insert_many(data, fields = fields).execute()
+query = User.select()
+for user in query:
+	print(user.email)
