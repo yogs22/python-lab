cd501d73c62d7b7aa338c56663b3431c27f4f0f1
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-02-12 06:09:57 +0700

    Peewee shorting data

--- database/user.py
+++ database/user.py
@@ -1,10 +1,12 @@
 from peewee import *
+import random, datetime
 
 sqlite_db = SqliteDatabase('user.db')
 
 class User(Model):
-	name = CharField()
-	email = CharField()
+	name    = CharField()
+	point   = IntegerField()
+	join_at = DateTimeField(default=datetime.datetime.now) 
 
 	class Meta:
 		database = sqlite_db
@@ -12,6 +14,19 @@ class User(Model):
 sqlite_db.connect()
 sqlite_db.create_tables([User], safe = True)
 
-query = User.select()
-for user in query:
-	print(user.email)
+users = User.select().order_by(User.point.desc())
+
+for user in users:
+	print(user.name + ' Memiliki ' + str(user.point) + ' Point \nbergabung pada', user.join_at)
+
+# def get_rand():
+# 	return random.randint(1,20)
+
+# data = [
+# 	{'name': 'hilman', 'point': get_rand()},
+# 	{'name': 'yogi', 'point': get_rand()},
+# 	{'name': 'novi', 'point': get_rand()},
+# 	{'name': 'aldy', 'point': get_rand()},
+# ]
+
+# User.insert_many(data).execute()
