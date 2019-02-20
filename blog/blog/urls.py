9b5c1dc83f3a95bcd9d33d72d85795652880acc0
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-02-21 06:46:09 +0700

    Django app consep

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+	path('blogs/', include('blogs.urls')),
 	path('', views.hello_world),
     path('admin/', admin.site.urls),
 ]
