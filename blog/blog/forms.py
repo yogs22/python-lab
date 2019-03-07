d4fed4c5a894290929bfe00839df240c42a8ba80
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-03-08 06:43:21 +0700

    Django form validator

--- blog/blog/forms.py
+++ blog/blog/forms.py
@@ -1,8 +1,18 @@
 from django import forms
+from django.core import validators
 
 class ContactForm(forms.Form):
 	name = forms.CharField()
 	email = forms.EmailField()
 	subject = forms.CharField(widget = forms.Textarea)
-
+	honeypot = forms.CharField(widget = forms.HiddenInput, required = False,
+		validators = [validators.MaxLengthValidator(0)])
 	
+	### This is custom validator
+	
+	# def clean_honeypot(self):
+	# 	honeypot = self.cleaned_data['honeypot']
+	# 	if len(honeypot):
+	# 		raise forms.ValidationError('Nggak boleh diisi')
+
+	# 	return honeypot
